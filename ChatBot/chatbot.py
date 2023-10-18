1a481332c530dfbea9e2d84318a13eda25a5d6a4
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 13:49:02 +0300

    ChatBot 2-nd stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,8 @@
+# chatbot.py
+import datetime
+
+bot_name = "DICT_Bot"
+birth_year = datetime.datetime.now().year
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {birth_year}.")
