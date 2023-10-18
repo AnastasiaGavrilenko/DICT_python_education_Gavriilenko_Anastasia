977fe5c5b91469f6bb9101061fc01eb5ef347f5d
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 14:14:37 +0300

    ChatBot 5-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -15,10 +15,18 @@ print(f"What a great name you have, Alex {user_name}!")
 print("Let me guess your age.")
 print("Enter remainders of dividing your age by 3, 5, and 7.")
 
-remainder3 = int(input(">  "))
-remainder5 = int(input(">  "))
-remainder7 = int(input(">  "))
+remainder3 = int(input("> "))
+remainder5 = int(input("> "))
+remainder7 = int(input("> "))
 
 your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
 print(f"Your age is {your_age}; that's a good time to start programming!")
+
+print("Now I will prove to you that I can count to any number you want.")
+number = int(input("> "))
+
+for i in range(number + 1):
+    print(f"{i} !")
+
+print("Completed, have a nice day!")
