3f10a27f681925d7629bc51d840018cdc679045b
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 13:54:58 +0300

    ChatBot 3-rd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -6,3 +6,8 @@ birth_year = datetime.datetime.now().year
 
 print(f"Hello! My name is {bot_name}.")
 print(f"I was created in {birth_year}.")
+
+print("Please, remind me your name.")
+user_name = input("> Alex ")
+
+print(f"What a great name you have, Alex {user_name}!")
